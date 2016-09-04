68f5a0f2d44f31831d07b345c51e9dfc7f408ca0
dahui58 <dahui58@hotmail.com>
2016-09-04 15:39:17 +0100

    Added CSV reading classes to read in planet data

---
+++ CSV.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class CSV {
+
+  private ArrayList<ArrayList<String>> data;
+  public final int rows;
+  public final int cols;
+
+  public CSV(ArrayList<ArrayList<String>> newRows) {
+    data = newRows;
+    rows = data.size();
+    cols = data.get(0).size();
+  }
+
+  public ArrayList<String> getZeroIndexedRow(int rowHeight) {
+    return data.get(rowHeight);
+  }
+
+}
--- Game.java
+++ Game.java
@@ -1,26 +1,47 @@
+import java.util.ArrayList;
+
 public class Game {
 
   public static void main(String[] args) {
-    System.out.println("Making planet Earth");
-    Planet earth = new Planet("Earth", new GridPoint(2,3),9);
 
-    System.out.println("Making planet Jupiter");
-    Planet jupiter = new Planet("Jupiter", new GridPoint(6,6),9);
-
-    System.out.println("Goods available at planet " + earth.name);
-    for (Goods g : earth.market.availableGoods) {
-      System.out.println(g.name + " " + g.getActualValue());
-    }
-
-    System.out.println("");
     System.out.println("Initialising scanner");
     Scanner scanner = new Scanner();
 
     GridMap map = GridMap.generateGridMap(11,7);
-    map.populateGridSquare(earth);
-    map.populateGridSquare(jupiter);
+
+    CSV planets = CSVReader.readCSV("planets");
+    for (int i = 1; i < planets.rows; i++) {
+      ArrayList<String> planet = planets.getZeroIndexedRow(i);
+      String name = planet.get(0);
+      int gridX = Integer.parseInt(planet.get(1));
+      int gridY = Integer.parseInt(planet.get(2));
+      int marketSize = Integer.parseInt(planet.get(3));
+
+      map.populateGridSquare(new Planet(
+                              name,
+                              new GridPoint(gridX, gridY),
+                              marketSize
+      ));
+      System.out.println("added " + name);
+    }
 
     scanner.scan(map);
+
+    // System.out.println("Making planet Earth");
+    // Planet earth = new Planet("Earth", new GridPoint(2,3),9);
+    //
+    // System.out.println("Making planet Jupiter");
+    // Planet jupiter = new Planet("Jupiter", new GridPoint(6,6),9);
+    //
+    // System.out.println("Goods available at planet " + earth.name);
+    // for (Goods g : earth.market.availableGoods) {
+    //   System.out.println(g.name + " " + g.getActualValue());
+    // }
+    //
+    // System.out.println("");
+    // map.populateGridSquare(earth);
+    // map.populateGridSquare(jupiter);
+    //
 
 
 
