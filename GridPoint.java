1a2999e72a4dc1628514dfc728f203ff1e6521cb
dahui58 <dahui58@hotmail.com>
2016-09-05 23:07:48 +0100

    Changed ints to doubles when calculating distance of diagonal journeys

--- GridPoint.java
+++ GridPoint.java
@@ -12,27 +12,36 @@ public class GridPoint {
     int xDist = getXDistance(point.x);
     int yDist = getYDistance(point.y);
     if (xDist == 0) {
-      System.out.println("No x distance, using vertical y distance");
+      System.out.println("No x distance, using vertical y distance" + yDist);
       return yDist;
     }
     if (yDist == 0) {
-      System.out.println("No y distance, using horizontal x distance");
+      System.out.println("No y distance, using horizontal x distance" + xDist);
       return xDist;
     }
-    int xSq = xDist*xDist;
-    int ySq = yDist*yDist;
+    double xSq = Math.pow((double)xDist,2);
+    double ySq = Math.pow((double)yDist,2);
     System.out.println("x sq = " + xSq);
     System.out.println("y sq = " + ySq);
-    return (int)(Math.sqrt(xSq+ySq));
+    System.out.println("sum = " + xSq + ySq);
+    System.out.println("double sqrt = " + Math.sqrt(xSq+ySq));
+    int sqi = (int) Math.sqrt(xSq+ySq);
+    System.out.println("int sqrt = " + sqi);
+    return sqi;
   }
 
   private int getXDistance(int pointX) {
-    if (x <= pointX) return pointX - x;
-    else return x - pointX;
+      int d = 0;
+      if (x <= pointX) d = pointX - x;
+      else if (x == pointX) d = 0;
+      else d = x - pointX;
+      System.out.println("X dist = " + d);
+      return d;
   }
 
   private int getYDistance(int pointY) {
     if (y <= pointY) return pointY - y;
+    else if (y == pointY) return 0;
     else return y - pointY;
   }
 }
