67ed16472fb7b5956f5df48b451096dc869156dc
dahui58 <dahui58@hotmail.com>
2016-09-15 22:04:05 +0100

    Player can now enter commands to scan, travel, or view goods available at any local planet

--- Game.java
+++ Game.java
@@ -37,60 +37,87 @@ public class Game {
         marketSize));
         //System.out.println("added " + name);
       }
-
+      boolean firstRun = true;
       while (true) {
           GridPoint l = p1.getLocation();
 
           String xLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
           System.out.println(p1.name + " is at " + xLetters.charAt(l.x) + "," + l.y);
-
-          p1.scan();
-
-          System.out.println("Select a square to travel to: ('A-Z','0-9'):");
-          System.out.print("X = ");
-          char xChar = 'B';
-          int y = 2;
+          String input = null;
+          System.out.print(">> ");
           try {
-              BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-              xChar = br.readLine().charAt(0);
-
-          } catch (Exception io) {
-              io.printStackTrace();
-          }
-          try {
-              System.out.print("Y = ");
-              BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-              y = Integer.parseInt(br.readLine());
+              BufferedReader commandReader = new BufferedReader(new InputStreamReader(System.in));
+              input = commandReader.readLine();
           } catch (Exception io) {
               io.printStackTrace();
           }
 
-          int x = xLetters.indexOf(xChar);
-
-          GridPoint destination = new GridPoint(x,y);
-          GridSquare destinationSquare = map.getSquareAt(destination);
-          int distance = p1.getLocation().comparePoints(destination);
-
-          boolean destinationIsAPlanet = destinationSquare instanceof Planet;
-
-          String destinationString;
-          if (destinationIsAPlanet) {
-              Planet planet = (Planet) destinationSquare;
-              destinationString = planet.name;
-          } else {
-              destinationString = "nowhere";
-          }
-
-          System.out.println("You travel " + distance + " to reach " + destinationString);
-          p1.setLocation(destination);
-          if (destinationIsAPlanet) {
-              Planet planet = (Planet) destinationSquare;
-              ArrayList<Goods> availableGoods = planet.market.availableGoods;
-              System.out.println("GOODS:");
-              for (Goods g : availableGoods) {
-                  System.out.println(" - " + g.name + " : " + g.getActualValue() + " CREDS   ");
+          if (input.equalsIgnoreCase("scan")) {
+              p1.scan();
+          } else if (input.equalsIgnoreCase("trade")) {
+              GridSquare location = map.getSquareAt(p1.getLocation());
+              if (location instanceof Planet) {
+                  Planet planet = (Planet) location;
+                  ArrayList<Goods> availableGoods = planet.market.availableGoods;
+                  System.out.println("GOODS:");
+                  for (Goods g : availableGoods) {
+                      System.out.println(" - " + g.name + " : " + g.getActualValue() + " CREDS   ");
+                  }
+              } else {
+                  System.out.println("Cannot trade here.");
               }
+          } else if (input.equalsIgnoreCase("travel")) {
+              System.out.println("Select a square to travel to: ('A-Z','0-9'):");
+              System.out.print("X = ");
+              char xChar = 'B';
+              int y = 2;
+              try {
+                  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                  xChar = br.readLine().charAt(0);
+
+              } catch (Exception io) {
+                  io.printStackTrace();
+              }
+              try {
+                  System.out.print("Y = ");
+                  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                  y = Integer.parseInt(br.readLine());
+              } catch (Exception io) {
+                  io.printStackTrace();
+              }
+
+              int x = xLetters.indexOf(xChar);
+
+              GridPoint destination = new GridPoint(x,y);
+              GridSquare destinationSquare = map.getSquareAt(destination);
+              int distance = p1.getLocation().comparePoints(destination);
+
+              boolean destinationIsAPlanet = destinationSquare instanceof Planet;
+
+              String destinationString;
+              if (destinationIsAPlanet) {
+                  Planet planet = (Planet) destinationSquare;
+                  destinationString = planet.name;
+              } else {
+                  destinationString = "nowhere";
+              }
+
+              System.out.println("You travel " + distance + " to reach " + destinationString);
+              p1.setLocation(destination);
+              if (destinationIsAPlanet) {
+                  Planet planet = (Planet) destinationSquare;
+                  ArrayList<Goods> availableGoods = planet.market.availableGoods;
+                  System.out.println("GOODS:");
+                  for (Goods g : availableGoods) {
+                      System.out.println(" - " + g.name + " : " + g.getActualValue() + " CREDS   ");
+                  }
+              }
+          } else {
+              System.out.println("Command \"" + input + "\" not recognised.");
+              System.out.println("Available commands: [scan] [trade] [travel]");
           }
+
+          try { Thread.sleep(2000); } catch (Exception e) { e.printStackTrace(); }
           System.out.println("");
       }
 
