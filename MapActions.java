512384fc5b1dbf3e62674ccacde3be7042d1e0d9
dahui58 <dahui58@hotmail.com>
2016-09-17 21:32:06 +0100

    Added an interface with the player commands on the map screen

---
+++ MapActions.java
@@ -0,0 +1,17 @@
+public interface MapActions {
+
+    void travel();
+
+    void shipStatus();
+
+    void trade();
+
+    void scan();
+
+    void waitWithBeacon();
+
+    void waitWithoutBeacon();
+
+    void help();
+
+}
