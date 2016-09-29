60075a84ff4bd235dbb95c89f18d02070f8802e1
dahui58 <dahui58@hotmail.com>
2016-09-29 22:51:27 +0100

    Goods now take a purchasedFrom GridPoint when being constructed.

--- goods/PurchasedGoods.java
+++ goods/PurchasedGoods.java
@@ -1,14 +1,17 @@
 package goods;
 
+import map.GridPoint;
+
 public class PurchasedGoods extends Goods {
 
     public final int purchasedValue;
-
+    public final GridPoint purchasedFrom;
     private int quantity;
 
-    public PurchasedGoods(GoodsForSale goods, int quantityPurchased) {
+    public PurchasedGoods(GoodsForSale goods, int quantityPurchased, GridPoint purchaseOrigin) {
         super(goods.id,goods.name,goods.legal,goods.baseValue);
         purchasedValue = goods.getPurchaseValue();
+        purchasedFrom = purchaseOrigin;
         quantity = quantityPurchased;
     }
 
