864cef58edef1429a8feda3e4138dbda35645480
dahui58 <dahui58@hotmail.com>
2016-10-03 20:55:12 +0100

    Added Session and View classes for simulating and viewing games

---
+++ arch/session/Session.java
@@ -0,0 +1,12 @@
+package arch.session;
+
+public class Session {
+
+    public final String name;
+    //private View view;
+
+    public Session(String sessionName) {
+        name = sessionName;
+    }
+
+}
---
+++ arch/view/View.java
@@ -0,0 +1,13 @@
+package arch.view;
+
+public interface View {
+
+    int getIntFromUser();
+
+    int getIntInRangeFromUser(int options);
+    
+    char getCharFromUser();
+
+    String getStringFromUser();
+
+}
