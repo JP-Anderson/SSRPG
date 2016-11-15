d7ce7839d03a60a5837b3c2f7ed715dc488288a3
dahui58 <dahui58@hotmail.com>
2016-11-15 18:15:48 +0000

    Updated ShipBattleSession and added ShipBattleView

--- arch/session/ShipBattleSession.java
+++ arch/session/ShipBattleSession.java
@@ -1,13 +1,30 @@
 package arch.session;
 
+import ship.AbstractShip;
+
 public class ShipBattleSession extends Session {
 
-    public ShipBattleSession() {
+    private final AbstractShip ship1;
+    private final AbstractShip ship2;
+
+    private ShipBattleView view;
+
+    public ShipBattleSession(AbstractShip newShip1, AbstractShip newShip2) {
         super("ShipBattleSession");
+        ship1 = newShip1;
+        ship2 = newShip2;
     }
 
     @Override
     public void run() {
+        //TODO need to replace System calls with output handler class
         System.out.println("Battle starting.");
+
+        // determine which ship runs first
+        // -- engine modules? weapon modules? pilot/gunner skill?
+
+        // or will it be real time battles, similar to FTL?
+
+        //
     }
 }
---
+++ arch/view/ShipBattleView.java
@@ -0,0 +1,9 @@
+package arch.view;
+
+public class ShipBattleView {
+
+    private static class ShipBattleConsole {
+        
+    }
+
+}
