454745233b2dae05380c43b58bb2a5278efd6fc4
dahui58 <dahui58@hotmail.com>
2016-11-19 16:23:00 +0000

    Amended incorrect comment on shields constructor

--- ship/shields/BasicShieldsMk2.java
+++ ship/shields/BasicShieldsMk2.java
@@ -3,8 +3,8 @@ package ship.shields;
 public class BasicShieldsMk2 extends ShieldSystem {
 
     public BasicShieldsMk2() {
-        //  Name, Cost, RechargeTime, RechargePerTurn, MaxShields, ReqPower)
-        super("BasicShieldsMk2", 200, 3, 40, 500,0.6, 1);
+        //  (Name, Cost, RechargeTime, RechargePerTurn, MaxShields, HullDamageAbsorption, ReqPower)
+        super("BasicShieldsMk2", 200, 3, 40, 500, 0.6, 1);
     }
 
 }
