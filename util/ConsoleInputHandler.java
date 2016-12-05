2483f7234ea786955ab816715ed15698181726bf
dahui58 <dahui58@hotmail.com>
2016-12-05 21:19:53 +0000

    Added comments to ConsoleInputHandler and moved the private functions up to the top due to their critical use in all other functions.

--- util/ConsoleInputHandler.java
+++ util/ConsoleInputHandler.java
@@ -3,13 +3,46 @@ package util;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/*
+    This class can be used to take user input through the consoles. It has a
+    number of functions for validating user input of various types.
+ */
 public class ConsoleInputHandler {
 
+    /*
+        Internal function which passes user input into the public functions
+    */
+    private static String readFromBufferedReader(String promptMessage) {
+        prompt(promptMessage);
+        String line = "";
+        try {
+            BufferedReader commandReader = new BufferedReader(new InputStreamReader(System.in));
+            line = commandReader.readLine();
+        } catch (Exception io) {
+            io.printStackTrace();
+        }
+        return line;
+    }
+
+    /*
+        Prints a provided String prompt to the user.
+        This function is called before taking any input.
+    */
+    private static void prompt(String promptMessage) {
+        System.out.print(promptMessage + ">> ");
+    }
+
+    /*
+        Returns the user input as a String.
+     */
     public static String getStringFromUser(String promptMessage) {
         String input = readFromBufferedReader(promptMessage);
         return input;
     }
 
+    /*
+        Only accepts a single lower or uppercase letter from the user.
+    */
     public static char getCharFromUser(String promptMessage) {
         while (true) {
             String input = readFromBufferedReader(promptMessage);
@@ -27,6 +60,9 @@ public class ConsoleInputHandler {
         }
     }
 
+    /*
+        Only accepts an integer from the user.
+    */
     public static Integer getIntFromUser(String promptMessage) {
         while (true) {
             String input = readFromBufferedReader(promptMessage);
@@ -40,6 +76,9 @@ public class ConsoleInputHandler {
         }
     }
 
+    /*
+        Only accepts an int in the range of 0 to the specified 'options' argument.
+    */
     public static int getIntInRangeFromUser(int options) {
         while (true) {
             int input = getIntFromUser("");
@@ -47,22 +86,6 @@ public class ConsoleInputHandler {
             else if (input == 0) return input;
             else System.out.println("Number out of range!");
         }
-    }
-
-    private static String readFromBufferedReader(String promptMessage) {
-        prompt(promptMessage);
-        String line = "";
-        try {
-            BufferedReader commandReader = new BufferedReader(new InputStreamReader(System.in));
-            line = commandReader.readLine();
-        } catch (Exception io) {
-            io.printStackTrace();
-        }
-        return line;
-    }
-
-    private static void prompt(String promptMessage) {
-        System.out.print(promptMessage + ">> ");
     }
 
 }
