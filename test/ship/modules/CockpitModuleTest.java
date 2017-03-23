30418a6e58509f4a652886793d30c6b16bf27bd8
dahui58 <dahui58@hotmail.com>
2017-03-23 00:09:15 +0000

    Added tests for CockpitModule dodging functionality

---
+++ test/ship/modules/CockpitModuleTest.java
@@ -0,0 +1,55 @@
+package ship.modules;
+
+import characters.Crewmember;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import ship.PlayerShip;
+import ship.cockpits.Cockpit;
+import util.rng.MockRandomNumberGenerator;
+import util.tests.PlayerShipTestHelper;
+import util.tests.ShipWeaponTestHelper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CockpitModuleTest {
+
+	@Test
+	void canDodgeAttack() {
+		PlayerShip playerShip = PlayerShipTestHelper.populateShipWithCockpitModule(
+				PlayerShipTestHelper.makeCrewWithPilot()
+		);
+
+		CockpitModule mod = playerShip.getCockpitModule();
+		mod.cockpit(new Cockpit(",",3,900,0.4));
+		Crewmember pilot = playerShip.getCrewmembersOfClass("Pilot").get(0);
+		mod.assignCrewmember(pilot);
+
+		MockRandomNumberGenerator rng = new MockRandomNumberGenerator();
+		rng.loadSingleRandZeroToOneDouble(0.2);
+
+		playerShip.sustainFire(ShipWeaponTestHelper.mockEnemyHeavyRocketAttack(), rng);
+
+		assertTrue(playerShip.getRemainingHullIntegrity() == playerShip.getMaxHullIntegrity());
+	}
+
+
+	@Test
+	void canDodgeAttackFailsIfRollIsAboveDodgeChance() {
+		PlayerShip playerShip = PlayerShipTestHelper.populateShipWithCockpitModule(
+				PlayerShipTestHelper.makeCrewWithPilot()
+		);
+
+		CockpitModule mod = playerShip.getCockpitModule();
+		mod.cockpit(new Cockpit(",",3,900,0.4));
+		Crewmember pilot = playerShip.getCrewmembersOfClass("Pilot").get(0);
+		mod.assignCrewmember(pilot);
+
+		MockRandomNumberGenerator rng = new MockRandomNumberGenerator();
+		rng.loadSingleRandZeroToOneDouble(0.8);
+
+		playerShip.sustainFire(ShipWeaponTestHelper.mockEnemyHeavyRocketAttack(), rng);
+
+		assertFalse(playerShip.getRemainingHullIntegrity() == playerShip.getMaxHullIntegrity());
+	}
+
+}
